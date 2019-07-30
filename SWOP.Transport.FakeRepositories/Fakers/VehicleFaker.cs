445faee98fce2320252c40aebf7a0a3071e92207
Marcin Sulecki <marcin.sulecki@altkom.pl>
2019-07-30 08:00:32 +0200

    Zmiana generowania numeru rejestracyjnego

--- SWOP.Transport.FakeRepositories/Fakers/VehicleFaker.cs
+++ SWOP.Transport.FakeRepositories/Fakers/VehicleFaker.cs
@@ -14,7 +14,7 @@ namespace SWOP.Transport.FakeRepositories.Fakers
             RuleFor(p => p.Id, f => f.IndexFaker);
             RuleFor(p => p.Model, f => f.Vehicle.Model());
             RuleFor(p => p.Brand, f => f.Vehicle.Manufacturer());
-            RuleFor(p => p.PlateNumber, f => f.Random.String(2) + f.Random.Number(5));
+            RuleFor(p => p.PlateNumber, f => f.Lorem.Letter(2).ToUpper() + f.Random.Number(1000, 9999).ToString());
             RuleFor(p => p.IsRemoved, f => f.Random.Bool(0.3f));
             Ignore(p => p.CreatedAt);
         }
