6968e5da3be3be89d65297b35a2cf144f699a780
Marcin Sulecki <marcin.sulecki@akademia.altkom.pl>
2019-08-19 15:04:56 +0200

    Dodanie DbEmployeeRepository

---
+++ SWOP.Transport.DbRepositories/DbEmployeeRepository.cs
@@ -0,0 +1,43 @@
+﻿using SWOP.Transport.IRepositories;
+using SWOP.Transport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWOP.Transport.DbRepositories
+{
+    public class DbEmployeeRepository : IEmployeeRepository
+    {
+        public void Add(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Authorize(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
